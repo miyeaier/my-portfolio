47e1a281163095f3e6f7e081e406b7917c29ca7f
miyeaier <miyesier@gmail.com>
2022-06-17 00:05:14 +0200

    add Tapbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,22 @@
-import "./topbar.scss"
+import "./topbar.scss";
 
-function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div>Topbar</div>
-  )
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            MiyesierKaerman
+          </a>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Topbar
