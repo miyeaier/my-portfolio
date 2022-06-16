50de785f7d989ad5f3d2b19c979e56dcad4208c8
miyeaier <miyesier@gmail.com>
2022-06-17 00:38:34 +0200

    add image and intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,28 @@
-import "./intro.scss"
+import "./intro.scss";
+import { useRef } from "react";
 
-function intro() {
+function Intro() {
+  const textRef = useRef();
+
   return (
-    <div>intro</div>
-  )
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Miyesier Kaerman</h1>
+          <h3>front-end developer</h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default intro
+export default Intro;
