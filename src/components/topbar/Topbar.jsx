90cc29ad60aa113eee285d8eecd647d66191285e
miyeaier <miyesier@gmail.com>
2022-06-18 16:31:03 +0200

    add contact github and linkdin on tapbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,19 @@
 import "./topbar.scss";
 
- function Topbar({ menuOpen, setMenuOpen }) {
+function Topbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-          Outsider.
+            Outsider.
           </a>
+          <div className="itemContainer">
+          <a href="https://github.com/miyeaier">Github</a>
+          </div>
+          <div className="itemContainer">
+          <a href="https://www.linkedin.com/in/miyesier-kaerman-257494136/">Linkedin</a>
+          </div>
         </div>
         <div className="right">
           <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
@@ -20,4 +26,4 @@ import "./topbar.scss";
     </div>
   );
 }
-export default Topbar
+export default Topbar;
