2f727c7342966813265d08e64623ff6a2aa5a189
miyeaier <miyesier@gmail.com>
2022-06-18 13:16:38 +0200

    add Testimonial and image

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,37 +1,30 @@
-import "./testimonials.scss"
+import "./testimonials.scss";
 
 function Testimonials() {
   const data = [
     {
       id: 1,
-      name: "Tom Durden",
-      title: "Senior Developer",
-      img:
-        "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        icon: "assets/linkedin.png",
-      desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+      name: "Gabriel Christner",
+      title: "CEO på Dreamify",
+      img: "assets/gbr-D.png",
+      icon: "assets/linkedin.png",
+      desc: "Miyeser gjorde sin praktik hos oss på Dreamify och jag var nöjd med både hennes insats och kompetens",
     },
     {
       id: 2,
       name: "Elux Yilisuya",
       title: "CEO of Lovalot",
-      img:
-        "assets/112.png",
-        icon: "assets/linkedin.png",
-      desc:
-        "Miyesier Kaerman is a dedicated and fun person to work with. She’s punctual and deliver task within the time frame. More over, she’s curious person with multi talents and always thinks outside the box. ",
+      img: "assets/112.png",
+      icon: "assets/linkedin.png",
+      desc: "Miyesier Kaerman is a dedicated and fun person to work with. She’s punctual and deliver task within the time frame. More over, she’s curious person with multi talents and always thinks outside the box. ",
       featured: true,
     },
     {
       id: 3,
-      name: "Martin Harold",
-      title: "CEO of ALBI",
-      img:
-        "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/linkedin.png",
-      desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+      name: "",
+      title: "",
+      img: "",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
     },
   ];
   return (
@@ -42,16 +35,10 @@ function Testimonials() {
           <div className={d.featured ? "card featured" : "card"}>
             <div className="top">
               <img src="assets/right-arrow.png" className="left" alt="" />
-              <img
-                className="user"
-                src={d.img}
-                alt=""
-              />
+              <img className="user" src={d.img} alt="" />
               <img className="right" src={d.icon} alt="" />
             </div>
-            <div className="center">
-              {d.desc}
-            </div>
+            <div className="center">{d.desc}</div>
             <div className="bottom">
               <h3>{d.name}</h3>
               <h4>{d.title}</h4>
@@ -63,4 +50,4 @@ function Testimonials() {
   );
 }
 
-export default Testimonials
+export default Testimonials;
